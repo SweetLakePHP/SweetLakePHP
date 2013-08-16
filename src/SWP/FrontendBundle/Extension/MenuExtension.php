bb9b86b432758c8cdbb1a4ff924aaceec9cdcd6e
Mitchel Verschoof <mitchel@future500.nl>
2013-08-16 14:47:43 +0200

    Added menu helper

---
+++ src/SWP/FrontendBundle/Extension/MenuExtension.php
@@ -0,0 +1,53 @@
+<?php
+namespace SWP\FrontendBundle\Extension;
+
+class MenuExtension extends \Twig_Extension
+{
+    protected $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'menuIsActive' => new \Twig_Function_Method($this, 'isActive'),
+        );
+    }
+
+    protected function listBundle()
+    {
+        $listBundle = array();
+
+        return $listBundle;
+    }
+
+    public function isActive( $item )
+    {
+        $request = $this->request;
+        $route   = $request->getRequest()->attributes->get('_route');
+
+        if ($item === $route) {
+            return true;
+        }
+
+        $listBundle = $this->listBundle();
+
+        if (isset($listBundle[$item])) {
+            foreach ($listBundle[$item] as $list => $listItem) {
+                if ($route === $listItem) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function getName()
+    {
+        return 'swp_menu_extension';
+    }
+}
---
+++ src/SWP/FrontendBundle/Service/RequestService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SWP\FrontendBundle\Service;
+
+class RequestService
+{
+    protected $container;
+
+    public function __construct( $container )
+    {
+        $this->container = $container;
+    }
+
+    public function getRequest()
+    {
+        return $this->container->get('request');
+    }
+}
