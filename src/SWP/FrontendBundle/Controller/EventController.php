8b3cc8414335e1eae75056f892174c8804134330
Mitchel Verschoof <mitchel@future500.nl>
2014-02-13 16:06:52 +0100

    Codecleanup

--- src/SWP/FrontendBundle/Controller/EventController.php
+++ src/SWP/FrontendBundle/Controller/EventController.php
@@ -2,11 +2,8 @@
 
 namespace SWP\FrontendBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class EventController extends Controller
